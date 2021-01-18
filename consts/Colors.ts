67b30b56c464157698147686cfc9ed106f0d65f3
Khanh <tran_khanh@outlook.com>
2021-01-18 20:57:50 +0700

    add color and layour

---
+++ consts/Colors.ts
@@ -0,0 +1,20 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+const emeraldGreen = '#50C878';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: emeraldGreen,
+    tabIconDefault: '#ccc',
+    tabIconSelected: emeraldGreen,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
