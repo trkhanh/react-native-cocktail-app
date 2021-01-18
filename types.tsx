3c497cd801a6934ed01b9487ceaf6ba240f4807b
Khanh <tran_khanh@outlook.com>
2021-01-18 20:46:47 +0700

    init

---
+++ types.tsx
@@ -0,0 +1,14 @@
+export type RootStackParamList = {
+    Root: undefined;
+    NotFound: undefined;
+}
+export type BottomTabPramList = {
+    Search: undefined;
+    Favourites: undefined;
+}
+export type TabOneParamList = {
+    TabOneScreen: undefined;
+}
+export type TabTwoPramList = {
+    TabTwoScreen: undefined;
+}
